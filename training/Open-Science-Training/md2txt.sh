85edd756069f148e256389b0697b5e597c95c214
CRBR-A <Cerbere.Ace@gmail.com>
2024-10-26 12:38:55 +0200

    removed script and added source file.

--- training/Open-Science-Training/md2txt.sh
+++
@@ -1,8 +0,0 @@
-find . -name "*.md" -print0 | while read -d $'\0' fullfile
-do
-  # rename file
-  path="${fullfile%/*}"
-  file="${fullfile##*/}"
-  # rename extension
-  `pandoc --eol=crlf -f gfm "$fullfile" -t gfm -o "${fullfile%.*}.txt"`
-done
